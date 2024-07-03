5a124ed9df256f555aa20b34342fa41d93316275
daBobe <96573813+daBobe@users.noreply.github.com>
2024-07-03 21:05:49 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const Compte = require('../models/compte');
+const Compte = require('../models/Compte');
 exports.createUser = async (req, res) => {
   try {
     // Create a new User
